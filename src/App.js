8a310910df51ec43f01f2fbd08a83820a2468788
bobtikhonov88 <bobtikhonov88@gmail.com>
2022-04-04 19:29:26 -0400

    New Test.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./containers/NotFound"
 import Conversion from "./containers/Conversion"
 import NavBar from "./containers/NavBar"
+import Test from "./container/Test"
 import './sass/main.scss';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={Conversion()} />
           <Route exact path='/notfound' element={NotFound()} />
+          <Route exact path='/test' element={Test()} />
         </Routes>
 </BrowserRouter>
   );
---
+++ src/containers/Test.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Test = () => (
+    <div className='test'>
+        <p className='test__paragraph'>
+            Hello
+        </p>
+    </div>
+);
+
+export default Test;
