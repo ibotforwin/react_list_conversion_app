6ac350f81cbec35d72a36cbda49bdbcb8b596788
bobtikhonov88 <bobtikhonov88@gmail.com>
2022-04-08 10:26:32 -0400

    User profile ,index [0] for now, generated

--- src/containers/UserProfile.js
+++ src/containers/UserProfile.js
@@ -1,8 +1,42 @@
 import React from "react"
 
 export default function UserProfile (){
+
+    const [data, setData] = React.useState(null)
+    const [loading, setLoading] = React.useState(true)
+    const [error, setError] = React.useState(null)
+   
+
+    React.useEffect(()=> {
+        fetch("https://randomuser.me/api/")
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+                throw response
+            })
+            .then(data=> {
+                setData(data)
+            })
+            .catch(error => {
+                console.error("Error fetching data: ", error)
+                setError(error)
+            })
+            .finally(()=> {
+                setLoading(false)
+            })
+    }, [])
+
+    if (loading) return "Loading"
+    if (error) return "Error!!"
+    
     return (
-        <h1>Working</h1>
+        <>
+            <img src={data.results[0].picture.large} alt="Portrait thumbnail" />
+            <h1>This is {data.results[0].name.first} {data.results[0].name.last}</h1>
+            <p>The e-mail address is {data.results[0].email} and this person is from {data.results[0].location.city}</p>
+            
+        </>
 
     )
 }
